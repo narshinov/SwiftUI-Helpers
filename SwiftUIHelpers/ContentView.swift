378c60c5978befceb0874a2a7520cbf88297fa12
Nikita Arshinov <narshinov01@gmail.com>
2024-02-29 16:38:14 +0300

    Initial commit

---
+++ SwiftUIHelpers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIHelpers
+//
+//  Created by Nikita Arshinov on 29.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIHelpers/SwiftUIHelpersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHelpersApp.swift
+//  SwiftUIHelpers
+//
+//  Created by Nikita Arshinov on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHelpersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
