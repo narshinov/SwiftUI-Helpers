4f74a3a61c277604d0ac208e4f15c889547b56cb
Nikita Arshinov <narshinov01@gmail.com>
2024-02-29 17:40:07 +0300

    Minor fix

--- SwiftUIHelpers/SwiftUIHelpersApp.swift
+++ SwiftUIHelpers/SwiftUIHelpersApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIHelpersApp: App {
     var body: some Scene {
         WindowGroup {
-            TextFieldExempleContentView()
+            ContentView()
         }
     }
 }
