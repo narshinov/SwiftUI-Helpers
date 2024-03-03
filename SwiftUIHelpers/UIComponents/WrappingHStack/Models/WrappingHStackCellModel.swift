66769f68daac5622c03c53a633d31d42bc6250e9
Nikita Arshinov <narshinov01@gmail.com>
2024-03-04 00:05:38 +0300

    Added wrappingHStack

---
+++ SwiftUIHelpers/UIComponents/WrappingHStack/Models/WrappingHStackCellModel.swift
@@ -0,0 +1,13 @@
+//
+//  WrappingHStackCellModel.swift
+//  SwiftUIHelpers
+//
+//  Created by Nikita Arshinov on 3.03.24.
+//
+
+import Foundation
+
+struct WrappingHStackCellModel: Hashable {
+    let type: InterestsType
+    var isSelected = false
+}
